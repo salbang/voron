436f961867ae4f05509bdfa807038c0616f87ae4
Jung, Jin Hyuk <12740762+salbang@users.noreply.github.com>
2023-07-19 17:09:56 +0900

    Update update_mcu_rpi_host.sh

--- CAN/update_mcu_rpi_host.sh
+++ CAN/update_mcu_rpi_host.sh
@@ -4,8 +4,8 @@ git stash
 git stash drop
 git pull
 git update-index --assume-unchanged klippy/mcu.py
+/home/pi/klipper/patch_after_update.sh
 make clean
 make -j4
 make flash
-/home/pi/klipper/patch_after_update.sh
 echo "FIRMWARE_RESTART" > ~/printer_data/comms/klippy.serial
