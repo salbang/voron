f69aeb4eae71ee6ddf833c65d7afbe3f56043078
Jung, Jin Hyuk <12740762+salbang@users.noreply.github.com>
2026-02-17 22:54:55 +0900

    Updated command for older RPi.

--- MKS PWC 3.0/set-gpio-14-down.sh
+++ MKS PWC 3.0/set-gpio-14-down.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/bash
 if [ "$1" == "poweroff" ] || [ "$1" == "halt" ]; then
     echo "Shutting down"
-    /usr/bin/pinctrl set 14 op pn dl
+    # /usr/bin/pinctrl set 14 op pn dl
+    /usr/bin/raspi-gpio set 14 op pn dl
 fi
